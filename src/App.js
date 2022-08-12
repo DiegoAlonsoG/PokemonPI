0f14f5db8fe1ffe4127b1eb7d0aac6e8935e6117
Diego Alonso <diegoalonso.gutierrez@outlook.com>
2022-08-12 09:46:18 -0500

    UPDATE clean code v.1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import React from "react";
 import { Route } from "react-router-dom";
 import Landing from './components/Landing/Landing';
-// import PokemonCards from './components/PokemonCards/PokemonCards';
-// import Nav from './components/Nav/Nav';
 import CreatePokemon from './components/CreatePokemon/CreatePokemon';
 import PokemonDetail from './components/PokemonDetail/PokemonDetail';
 import Home from './components/Home/Home';
@@ -14,11 +12,7 @@ function App() {
     <div className="App">
       <Route exact path="/" component={Landing} />
       <Route exact path="/home" component={Home} />
-      {/* <Route exact path="/home" component={PokemonCards} /> */}
       <Route exact path="/create" component={CreatePokemon} />
-      {/* <Route 
-        exact path="/pokemon/:id" 
-        render={({match})=> <PokemonDetail match={match}/>}/> */}
       <Route exact path="/pokemon/:id" component={PokemonDetail} />
     </div>
   );
